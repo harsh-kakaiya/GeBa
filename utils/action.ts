c0da4cd98da9b5bdad098b150eda07671a1b8803
Harsh Kakaiya <80671616+harsh-kakaiya@users.noreply.github.com>
2024-05-20 16:58:42 +0530

    Update action.ts

--- utils/action.ts
+++ utils/action.ts
@@ -5,7 +5,7 @@ interface Chat {
     parts: string;
 }
 
-const genAI = new GoogleGenerativeAI("AIzaSyDDhgNuf1TZg1LTi_5gbB_cNa-TEf_cNBo")
+// const genAI = new GoogleGenerativeAI("API-KEY")
 
 export async function run(prompt:string, history: Chat[]) {
     const model=genAI.getGenerativeModel({model:"gemini-pro"})
